f4a7a1bb3184c515c35c8c718fd651f29d881d35
PedroKluivert <pedrokluivertpedro@gmail.com>
2019-01-22 11:53:46 -0300

    giroscópio
    o giroscópio está funcionando

--- sala_3.py
+++ sala_3.py
@@ -16,27 +16,23 @@ cor = ColorSensor('in3')
 cor.mode = 'COL-COLOR'
 gy = GyroSensor('in2')
 gy.mode = 'GYRO-ANG'
-L = [gy.value()]
+veloC = 500
+l=[]
 
-def Corregir(Lista):
-    if Lista[0] > gy.value():
-        lado = 'direita'
-        dife = Lista[0] - gy.value()
-        motor_dir.run_to_rel_pos(position_sp=dife, speed_sp=200)
-    elif Lista[0] < gy.value():
-        lado='esquerda'
-        dife = gy.value() - Lista[0]
-        motor_esq.run_to_rel_pos(position_sp=dife, speed_sp=200)
+button = Button()
 
-ve = 400
-i = 0
-while True:
-    i += 1
-    if cor.value()==1:
-        print('{} valor: {}'.format(i,gy.value))
-        motor_esq.run_forever(speed_sp=ve)
-        motor_dir.run_forever(speed_sp=ve)
-        dist = us.value()/10
-        print("while: ", cor.value())
-    if cor.value()!=1:
-        Corregir(L)
+'''while not button.any():
+    motor_dir.run_forever(speed_sp=400)
+    motor_esq.run_forever(speed_sp=400)
+    sleep(2)
+    l.append(gy.value())'''
+ant = gy.value()
+giro = 0
+while giro <=80:
+    giro=gy.value() - ant
+    print(giro,gy.value(),ant)
+    motor_dir.run_forever(speed_sp=400)
+    motor_esq.run_forever(speed_sp=0)
+motor_dir.stop()
+
+
