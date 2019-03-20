e1ee6756c33908266321ff987b7307e2414c7275
Isaque Melo <isaquegbmelo2@gmail.com>
2019-03-20 15:07:26 -0300

    tentativa de indentificar o local onde deixar as bolas resgatadas
    Fizemos uma lista com as distancias das 3 quinas e pedimos para o robo analisar se as distancias sao as mesmas, se nao for, significa que algo esta na frente, ou seja, o lugar onde deixar as bolas que foram resgatadas. O programa nao esta sendo executado perfeitamente, pois o sensor de distancia (ultrasonico) nao esta mostrando as distancias exatas

--- sala_3.py
+++ sala_3.py
@@ -12,34 +12,43 @@ motor_esq = LargeMotor('outB')
 motor_dir = LargeMotor('outC')
 motor_med = MediumMotor('outA')
 
+
 def Andar_tras():
-    motor_dir.run_forever(speed_sp = 500)
-    motor_esq.run_forever(speed_sp = 500)
+    motor_dir.run_forever(speed_sp=500)
+    motor_esq.run_forever(speed_sp=500)
+
 
 def Andar_frente():
-    motor_dir.run_forever(speed_sp = -500)
-    motor_esq.run_forever(speed_sp = -500)
+    motor_dir.run_forever(speed_sp=-500)
+    motor_esq.run_forever(speed_sp=-500)
+
 
 def Girar_esq():
-    motor_dir.run_forever(speed_sp = -500)
-    motor_esq.run_forever(speed_sp = 0)
+    motor_dir.run_forever(speed_sp=-500)
+    motor_esq.run_forever(speed_sp=0)
+
 
 def Girar_dir():
-    motor_dir.run_forever(speed_sp = 0)
-    motor_esq.run_forever(speed_sp = -500)
+    motor_dir.run_forever(speed_sp=0)
+    motor_esq.run_forever(speed_sp=-500)
+
 
 def Parar():
     motor_esq.stop()
     motor_dir.stop()
 
+
 def Subir_garra():
     motor_med.run_forever(speed_sp=250)
+
 
 def Descer_garra():
     motor_med.run_forever(speed_sp=-250)
 
+
 def Parar_garra():
     motor_med.stop()
+
 
 def Girar_dir_90():
     Giro_Total = 0
@@ -49,6 +58,7 @@ def Girar_dir_90():
         Giro_Total = gy.value() - Ang_ant
     Parar()
 
+
 def Girar_esq_90():
     Giro_Total = 0
     Ang_ant = gy.value()
@@ -56,8 +66,17 @@ def Girar_esq_90():
         Girar_esq()
         Giro_Total = Ang_ant - gy.value()
     Parar()
-'''
-Andar_frente()
+
+def Girar_esq_45():
+    Giro_Total = 0
+    Ang_ant = gy.value()
+    while (Giro_Total < 45):
+        Girar_esq()
+        Giro_Total = Ang_ant - gy.value()
+    Parar()
+
+
+'''Andar_frente()
 sleep(4.5)
 Parar()
 sleep(2)
@@ -78,23 +97,22 @@ motor_esq.run_forever(speed_sp=500)
 sleep(3)
 Andar_tras()
 sleep(1)
-Parar()'''
-
-
-#Percorrer a sala em "Zigue-Zague"
+Parar()
+'''
+'''# Percorrer a sala em "Zigue-Zague"
 for i in range(2):
-    if i==0:
+    if i == 0:
         Girar_dir_90()
-    distancia = us.value()/10
-    while(distancia > 8):
+    distancia = us.value() / 10
+    while (distancia > 8):
         Andar_tras()
-        distancia = us.value()/10
+        distancia = us.value() / 10
     Parar()
     sleep(1)
-    distancia = us.value()/10
-    while(distancia < 82):
+    distancia = us.value() / 10
+    while (distancia < 82):
         Andar_frente()
-        distancia = us.value()/10
+        distancia = us.value() / 10
     Parar()
     Andar_tras()
     sleep(1)
@@ -108,13 +126,13 @@ for i in range(2):
     sleep(1)
     Descer_garra()
     sleep(1)
-    while(distancia < 82):
+    while (distancia < 82):
         Andar_frente()
-        distancia = us.value()/10
-    distancia = us.value()/10
-    while(distancia < 82):
+        distancia = us.value() / 10
+    distancia = us.value() / 10
+    while (distancia < 82):
         Andar_frente()
-        distancia = us.value()/10
+        distancia = us.value() / 10
     Parar()
     Andar_tras()
     sleep(1)
@@ -125,4 +143,24 @@ for i in range(2):
     while (distancia < 82):
         Andar_frente()
         distancia = us.value() / 10
-    Parar()
+    Parar()'''
+
+L1=[75,80,75]
+Subir_garra()
+L2 = []
+preto =0
+for i in range(3):
+    if i ==0:
+        Girar_esq_90()
+        sleep(2)
+        L2.append(us.value()/10)
+    else:
+        Girar_esq_45()
+        sleep(2)
+        L2.append(us.value()/10)
+    if (L2[i] < L1[i]):
+        preto = i+1
+
+print(L1)
+print(L2)
+print(preto)
