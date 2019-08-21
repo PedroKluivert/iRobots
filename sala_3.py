430addbc8a115857863e1611d39e56e1d8da557b
GeovanaStefani <geovanastefani2011@gmail.com>
2019-08-21 16:56:37 -0300

    Sala_3 estrategia de visualizar a area de resgate.
    Tentamos enxergar atraves do ultrassonico a area de resgate nas diferentes posicoes da sala. Com falhas ainda.

--- sala_3.py
+++ sala_3.py
@@ -1,59 +1,115 @@
 #!/usr/bin/env python3
 
 from ev3dev.ev3 import *
-#from time import sleep
+from time import sleep
 import time
 import datetime
+from simple_pid import PID
 
-gy = GyroSensor('in2')
-gy.mode = 'GYRO-ANG'
-u = gy.units
-us = UltrasonicSensor('in4')
+# gy = GyroSensor('in2')
+# gy.mode = 'GYRO-ANG'
+# u = gy.units
+us = UltrasonicSensor('in1')
 us.mode = 'US-DIST-CM'
 motor_esq = LargeMotor('outB')
 motor_dir = LargeMotor('outC')
-motor_med = MediumMotor('outA')
-cl = ColorSensor('in3')
-cl.mode = 'COL-COLOR'
+motor_med = MediumMotor('outD')
+us2 = UltrasonicSensor('in3')
+us2.mode = 'US-DIST-CM'
 
-#Função andar para frente por 2.5seg
+#kp = 178 kd=0.8 ki=0.50 valor default
+pid = PID(150, 0, 0, setpoint=4)
 
-def Andar_frente():
+# cl = ColorSensor('in3')
+# cl.mode = 'COL-COLOR'
+# Função andar para frente por 2.5seg
+
+def Motor_Andar(temp, dir, esq=600):
     begin = datetime.datetime.now()
-    end = begin + datetime.timedelta(seconds=3.0)
+    end = begin + datetime.timedelta(seconds=temp)
     while datetime.datetime.now() <= end:
-        motor_esq.run_forever(speed_sp=600)
-        motor_dir.run_forever(speed_sp=600)
+        motor_esq.run_forever(speed_sp=esq)
+        motor_dir.run_forever(speed_sp=dir)
     Parar()
 
-#Função andar para frente por 1seg
-def Andar_2():
-    begin = datetime.datetime.now()
-    end = begin + datetime.timedelta(seconds=1.7)
-    while datetime.datetime.now() <= end:
-        motor_esq.run_forever(speed_sp=600)
-        motor_dir.run_forever(speed_sp=600)
-    Parar()
+#Nao ta funcionando essa funcao de giro
+def girar(sentido, giro=360, velocidade=300):
+    if(sentido == 'esquerda'):
+        motor_esq.run_to_rel_pos(position_sp=giro, speed_sp=300)
+        motor_dir.run_to_rel_pos(position_sp=-giro, speed_sp=-300)
+    if(sentido == 'direita'):
+        motor_esq.run_to_rel_pos(position_sp=-giro, speed_sp=-300)
+        motor_dir.run_to_rel_pos(position_sp=giro, speed_sp=300)
 
-#Função andar para trás por 1seg
-def Andar_3():
-    begin = datetime.datetime.now()
-    end = begin + datetime.timedelta(seconds=1)
-    while datetime.datetime.now() <= end:
-        motor_esq.run_forever(speed_sp=-600)
-        motor_dir.run_forever(speed_sp=-600)
-    Parar()
+def GirarGiroscopio(sentido, grau):
+    Total = 0
+    if(sentido == 'esquerda'):
+        Ang_ant = gy.value()
+        while(Total < grau):
+            motor_esq.run_forever(speed_sp=300)
+            motor_dir.run_forever(speed_sp=-300)
+            Total = gy.value() - Ang_ant
+        Parar()
+    if(sentido == 'direita'):
+        Ang_ant = gy.value()
+        while (Total < grau):
+            motor_esq.run_forever(speed_sp=-300)
+            motor_dir.run_forever(speed_sp=300)
+            Total = Ang_ant - gy.value()
+        Parar()
 
-#Função andar para trás por 2.5seg
-def Andar_tras():
-    begin = datetime.datetime.now()
-    end = begin + datetime.timedelta(seconds=2.5)
-    while datetime.datetime.now() <= end:
-        motor_esq.run_forever(speed_sp=-600)
-        motor_dir.run_forever(speed_sp=-600)
-    Parar()
 
-#Girar para direita
+# Função para andar infinito
+def Motor_Infinito_frente(x):
+
+    #control = pid(us.value())
+    #print(control)
+
+    while us.value() / 10 > x:
+
+        control = pid(us2.value()/10)
+        if control > 500:
+            control = 500
+        elif control < -500:
+            control = -500
+
+        print(control + 500, 500 - control)
+
+        motor_dir.run_forever(speed_sp=500 + control)
+        motor_esq.run_forever(speed_sp=500 - control)
+
+        if us.value() / 10 < 12: #distancia da parede
+             Parar()
+
+
+
+def Motor_Infinito_tras():
+    while us.value() / 10 > 15:
+        motor_dir.run_forever(speed_sp=-500)
+        motor_esq.run_forever(speed_sp=-500)
+        if us.value() / 10 < 10:
+            Parar()
+
+
+# Função para parar
+def Parar():
+    motor_esq.stop()
+    motor_dir.stop()
+
+# Função para subir a garra
+def Subir_garra():
+    motor_med.run_forever(speed_sp=-450)
+    sleep(2)
+
+# Função para descer a garra
+def Descer_garra():
+    motor_med.run_forever(speed_sp=450)
+    sleep(2)
+
+# Função para parar a garra
+def Parar_garra():
+    motor_med.stop()
+
 def Girar_dir():
     begin = datetime.datetime.now()
     end = begin + datetime.timedelta(seconds=1.25)
@@ -61,16 +117,6 @@ def Girar_dir():
         motor_esq.run_forever(speed_sp=0)
         motor_dir.run_forever(speed_sp=-500)
     Parar()
-
-def Girar_dir_eixo_180():
-    begin = datetime.datetime.now()
-    end = begin + datetime.timedelta(seconds=2.5)
-    while datetime.datetime.now() <= end:
-        motor_esq.run_forever(speed_sp=0)
-        motor_dir.run_forever(speed_sp=-600)
-    Parar()
-
-#Função girar para esquerda
 
 def Girar_esq():
     begin = datetime.datetime.now()
@@ -80,24 +126,6 @@ def Girar_esq():
         motor_dir.run_forever(speed_sp=0)
     Parar()
 
-#Função para parar
-def Parar():
-    motor_esq.stop()
-    motor_dir.stop()
-
-#Função para subir a garra
-def Subir_garra():
-    motor_med.run_forever(speed_sp=250)
-
-#Função para descer a garra
-def Descer_garra():
-    motor_med.run_forever(speed_sp=-250)
-
-#Função para parar a garra
-def Parar_garra():
-    motor_med.stop()
-
-#Função para girar para direita em 90 graus
 def Girar_dir_90():
     Giro_Total = 0
     Ang_ant = gy.value()
@@ -106,7 +134,6 @@ def Girar_dir_90():
         Giro_Total = gy.value() - Ang_ant
     Parar()
 
-#Função para girar para esquerda em 90 graus
 def Girar_esq_90():
     Giro_Total = 0
     Ang_ant = gy.value()
@@ -115,178 +142,69 @@ def Girar_esq_90():
         Giro_Total = Ang_ant - gy.value()
     Parar()
 
-#Função para girar para esquerda em 45 graus
-def Girar_esq_45():
-    Giro_Total = 0
-    Ang_ant = gy.value()
-    while (Giro_Total < 45):
-        Girar_esq()
-        Giro_Total = gy.value() - Ang_ant
-    Parar()
-
-#Função iniciar para saber por qual entrada o robô vai entrar
-def entrada():
-    print(us.value())
-    if us.value()>=20:
-        direita()
-    else:
-        esquerda()
-
-#Função para indentificar a área de resgate se entrar pela direita
-def direita():
-    cont=0
-    for i in range(3):
-        Andar_frente()
+def Percorrer():
+    volta = 0
+    for i in range(4):
+        Descer_garra()
         Parar()
-        Girar_esq()
-        print(cl.value())
-        if(cl.value()==1):
-            cont+=1
-        else:
-            print('Nao esta nessa ponta.')
-        Andar_2()
-        Girar_esq()
-    Andar_frente()
-    if(cont==1):
-        print('Zigue-zague1')
-        #contador1()
-    elif(cont==2):
-        print('Zigue-zague2')
-        #contador2()
-    elif(cont==3):
-        print('Zigue-zague3')
-        #contador3()
-    else:
-        print('A area de resgate nao foi detectada')
+        Motor_Andar(4.8, 650, 650) #andar frente
+        sleep(1)
+        Motor_Andar(1, -200, -200)
+        Subir_garra()
+        if(volta == 0):
+            if i ==3:
+                Parar()
+            Motor_Andar(6, 400, 0)
+            volta = 1
+        elif(volta == 1):
+            if i ==3:
+                Parar()
+            Motor_Andar(6, 0, 400)
+            volta = 0
+        Motor_Andar(2, -400, -400)
+
+CONSTANTE = True
+while CONSTANTE:
+    try:
+         Subir_garra()
+         dist = us.value()/10
+         Motor_Infinito_frente(12)
+         # Motor_Andar(1.5, 400, -400) #girar eixo 90
+         Motor_Andar(2.77, 400, 0)  # girar eixo 90
+         sleep(0.1)
+         # Motor_Andar(0.5, 600)       #andar para frente para analisar se é mesmo a área de resgate
+         sleep(1)
+         dist2 = us2.value() / 10
+         print(dist2, ' cm')
+         if(dist2 > 20):
+            Parar()
+            Sound.beep()
+            print('Vi a area de resgate!')
+            Percorrer()
+            break
+         else:
+            Subir_garra()
+            dist = us.value() / 10
+            Motor_Infinito_frente(18)
+            Motor_Andar(1.3, -200, -200)  # ré
+            Motor_Andar(1.4, 400, -400)
+            sleep(0.1)
+            Motor_Andar(1, 600)
 
 
-#Função para indentificar a área de resgate se entrar pela esquerda
-def esquerda():
-    Girar_esq_90()
-    cont = 0
-    for i in range(3):
-        Andar_tras()
-        Girar_dir()
-        print(cl.value())
-        if cl.value() == 1:
-            cont += 1
-        Andar_3()
-    Andar_tras()
-    if cont==1:
-        print('zigue-zague4')
-        #contador4()
-    elif cont==2:
-        print('zigue-zague5')
-        #contador5()
-    elif cont==3:
-        print('zigue-zague6')
-        #contador6()
+    except KeyboardInterrupt:
+        Parar()
+        Parar_garra()
+        CONSTANTE = False
+print('\nVoces vao conseguir!')
 
-
-entrada()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-'''Andar_frente()
-sleep(4.5)
-Parar()
-sleep(2)
-Andar_tras()
-sleep(1)
-Parar()
-Subir_garra()
-sleep(1)
-Descer_garra()
-sleep(1)
-motor_med.stop()
-Girar_esq()
-sleep(2)
-Andar_frente()
-sleep(4)
-motor_dir.run_forever(speed_sp=0)
-motor_esq.run_forever(speed_sp=500)
-sleep(3)
-Andar_tras()
-sleep(1)
-Parar()
-'''
-'''# Percorrer a sala em "Zigue-Zague"
-for i in range(2):
-    if i == 0:
-        Girar_dir_90()
-    distancia = us.value() / 10
-    while (distancia > 8):
-        Andar_tras()
-        distancia = us.value() / 10
-    Parar()
-    sleep(1)
-    distancia = us.value() / 10
-    while (distancia < 82):
-        Andar_frente()
-        distancia = us.value() / 10
-    Parar()
-    Andar_tras()
-    sleep(1)
-    Parar()
-    sleep(1)
-    Subir_garra()
-    sleep(1)
-    Girar_esq_90()
-    Girar_esq_90()
-    Andar_tras()
-    sleep(1)
-    Descer_garra()
-    sleep(1)
-    while (distancia < 82):
-        Andar_frente()
-        distancia = us.value() / 10
-    distancia = us.value() / 10
-    while (distancia < 82):
-        Andar_frente()
-        distancia = us.value() / 10
-    Parar()
-    Andar_tras()
-    sleep(1)
-    Subir_garra()
-    Girar_dir_90()
-    Girar_dir_90()
-    Descer_garra()
-    while (distancia < 82):
-        Andar_frente()
-        distancia = us.value() / 10
-    Parar()'''
-'''L1=[75,80,75]
-Subir_garra()
-L2 = []
-preto =0
-for i in range(3):
-    if i ==0:
-        Girar_esq_90()
-        sleep(2)
-        L2.append(us.value()/10)
-    else:
-        Girar_esq_45()
-        sleep(2)
-        L2.append(us.value()/10)
-    if (L2[i] < L1[i]):
-        preto = i+1
-
-print(L1)
-print(L2)
-print(preto)'''
+# for i in range(3):
+#     motor_esq.run_forever(speed_sp=-1000)
+#     motor_dir.run_forever(speed_sp=-1000)
+#     Parar()
+#     motor_esq.run_forever(speed_sp=300)
+#     motor_dir.run_forever(speed_sp=300)
+#     sleep(1)
+#     Parar()
+# Parar_garra()
 
